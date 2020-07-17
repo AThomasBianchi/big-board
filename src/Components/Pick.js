c13cf57e802aca75be5c07f83d2c02101c5e63ce
A Thomas Bianchi <atbianchi@gmail.com>
2020-07-16 22:10:56 -0400

    remove styling for ipad build

--- src/Components/Pick.js
+++ src/Components/Pick.js
@@ -4,14 +4,9 @@ import './Pick.css';
 const Pick = props => {
   return (
     <div className="pick" >
-      <div className="pick__round-spacing"></div>
-      <div className="pick__team-info" >
-        <div>{props.pick.team}</div>
-      </div>
-      <div className="pick__number-info" >
-          <div className="pick__number-round">{props.roundPick}</div>
-          <div className="pick__number-overall">{props.pickNo}</div>
-      </div>
+      <div className="pick__team-info" >{props.pick.team}</div>
+      <div className="pick__number-round">{props.roundPick}</div>
+      <div className="pick__number-overall">{props.pickNo}</div>
     </div>
   );
 };
--- src/Components/Round.js
+++ src/Components/Round.js
@@ -4,15 +4,7 @@ import './Round.css';
 const Round = props => {
   return (
     <div className="round">
-      <div className="round__label-container">
-        <div className="round__label">
-          <div>Round</div>
-          <div>{props.round}</div>
-        </div>
-      </div>
-      <div className="round__pick-container">
         {props.children}
-      </div>
     </div>
   );
 };
